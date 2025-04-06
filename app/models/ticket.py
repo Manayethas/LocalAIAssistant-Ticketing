ad395a6037369164e81ffad6d70f9e255a4d1fdb
Manayethas <brendanphillips97@gmail.com>
2025-04-06 03:01:24 -0600

    Update ticket.py

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -4,13 +4,14 @@ from datetime import datetime
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ticket_id = db.Column(db.String(100), unique=True, nullable=False)
+    issue = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(50), default="open")
+    last_updated = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # New fields you need to add:
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     email = db.Column(db.String(120))
     username = db.Column(db.String(100))
     ip_address = db.Column(db.String(50))
     user_agent = db.Column(db.Text)
-    issue = db.Column(db.String(255), nullable=False)
-    status = db.Column(db.String(50), default="open")
-    last_updated = db.Column(db.DateTime, default=datetime.utcnow)
-    
